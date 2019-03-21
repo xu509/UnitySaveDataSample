236cbc2d176a29fe5db887b41857ab00c05deb11
xuningxin <xu509@outlook.com>
2019-03-22 00:36:36 +0800

    11

---
+++ Assets/UnitySaveDataSampleAsset/example01/PlayerPrefsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsScript : MonoBehaviour {
+
+	private GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		cube = GameObject.Find ("Cube");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerPrefs playerPrefs = new PlayerPrefs ();
+	}
+}
